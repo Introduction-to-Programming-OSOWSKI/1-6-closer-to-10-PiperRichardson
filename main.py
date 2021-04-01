e47834ffd73ec23008e1b70192328004f6d29e77
Richardson Piper <817420@stma.k12.mn.us>
2021-04-01 10:04:58 -0500

    Closer to 10 try one

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#WRITE YOUR CODE IN THIS FILE
+def close10(x, y, z):
+    if abs(10 - x) < abs(10 - y):
+        return x
+    elif abs(10 - x) > abs(10 - y):
+        return y 
+    else abs(x) == abs(y)
+        return 0
